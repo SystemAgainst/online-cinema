d5269cfeac318a73f5b1c797a98b1cf10ca56a12
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-06 20:56:08 +0300

    add: realized dto for auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
   @Post('register')
   async register(@Body() dto: any) {
     return this.authService.register(dto);
