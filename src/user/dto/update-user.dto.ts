82115489f9c36fde69bda2e9bb3ff62bd50d5f42
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-09 22:31:42 +0300

    add: realized dto to update user data

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+	@IsEmail()
+	email: string;
+
+	@IsString()
+	@MinLength(6, {
+		message: 'Password can not be less than 6 symbols',
+	})
+	password: string;
+
+	isAdmin?: boolean;
+}
