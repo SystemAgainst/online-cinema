18452772d1488970f56ba75d866f9a4cf8d2da4f
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-09 22:01:51 +0300

    add: realized custom decorator @Auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from '../auth/decorators/auth.decorator';
 
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
 	@Get('profile')
-	// @Auth
+	@Auth('admin')
 	async getProfile() {
 		return this.userService.getDataById();
 	}
