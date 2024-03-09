69e093ae9e98310ba82db08a5dbce8a5c083c483
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-09 21:52:45 +0300

    add: realized guards for authorized user and admin

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { UserEntity } from './user.entity';
 
 @Module({
 	controllers: [UserController],
 	providers: [UserService],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserEntity,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+	],
 })
 export class UserModule {}
