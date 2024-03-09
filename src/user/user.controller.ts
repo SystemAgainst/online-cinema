f4acad3ef7edd0bb4f45e5ec1a9acb61196aa885
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-09 23:01:16 +0300

    add: realized method that let admin change/update user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import {
 	Controller,
 	Get,
 	HttpCode,
+	Param,
 	Put,
 	UsePipes,
 	ValidationPipe,
@@ -11,6 +12,7 @@ import { UserService } from './user.service';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { User } from './decorators/user.decorator';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { IdValidationPipe } from '../pipes/id.validation.pipe';
 
 @Controller('users')
 export class UserController {
@@ -28,5 +30,16 @@ export class UserController {
 	@Auth()
 	async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
 		return this.userService.updateProfile(_id, dto);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async updateUser(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() data: UpdateUserDto
+	) {
+		return this.userService.updateProfile(id, data);
 	}
 }
