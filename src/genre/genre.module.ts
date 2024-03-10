2402c02f249c9a97c4104426829b330f1628cfef
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-10 10:42:41 +0300

    add: realized module for genre

--- src/genre/genre.module.ts
+++ src/genre/genre.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GenreService } from './genre.service';
 import { GenreController } from './genre.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { GenreEntity } from './genre.entity';
 
 @Module({
-  controllers: [GenreController],
-  providers: [GenreService],
+	controllers: [GenreController],
+	providers: [GenreService],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: GenreEntity,
+				schemaOptions: {
+					collection: 'Genre',
+				},
+			},
+		]),
+	],
 })
 export class GenreModule {}
