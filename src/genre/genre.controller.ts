30218adee2d9135370a6f7629fb24cc97a2520f4
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-10 21:12:39 +0300

    add: get genre by id (draft)

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GenreService } from './genre.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { IdValidationPipe } from '../pipes/id.validation.pipe';
 
 @Controller('genre')
 export class GenreController {
-  constructor(private readonly genreService: GenreService) {}
+	constructor(private readonly genreService: GenreService) {}
+
+	@Get(':id')
+	@Auth('admin')
+	async getGenre(@Param('id', IdValidationPipe) id: string) {
+		return this.genreService.getById(id);
+	}
+
+	// @UsePipes(new ValidationPipe())
+	// @Post()
+	// @HttpCode(200)
+	// @Auth('admin')
+	// async createGenre() {
+	// 	return this.genreService.createGenre();
+	// }
 }
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { GenreEntity } from './genre.entity';
 
 @Injectable()
-export class GenreService {}
+export class GenreService {
+	constructor(
+		@InjectModel(GenreEntity)
+		private readonly genreEntity: ModelType<GenreEntity>
+	) {}
+
+	async getById(_id: string) {
+		const user = await this.genreEntity.findById(_id);
+
+		if (!user) throw new NotFoundException('User not found');
+
+		return user;
+	}
+
+	// async createGenre() {
+	// 	const defaultValue: CreateGenreDto = {
+	// 		name: '',
+	// 		slug: '',
+	// 		description: '',
+	// 		icon: '',
+	// 	};
+	//
+	// 	const genre = await this.genreEntity.create(defaultValue);
+	//
+	// 	return genre._id;
+	// }
+}
