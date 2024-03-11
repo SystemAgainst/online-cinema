85e918c65c04713313aa63862ea75cae83fc84b0
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-11 21:04:06 +0300

    add: added file upload functionality

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	HttpCode,
+	Post,
+	Query,
+	UploadedFile,
+	UseInterceptors,
+} from '@nestjs/common';
 import { FilesService } from './files.service';
+import { Auth } from '../auth/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { IFileResponse } from './interfaces/files.interface';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+	constructor(private readonly filesService: FilesService) {}
+
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	@UseInterceptors(FileInterceptor('image'))
+	async uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Query('folder') folder?: string
+	): Promise<IFileResponse[] | undefined> {
+		return this.filesService.saveFiles([file], folder);
+	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { IFileResponse } from './interfaces/files.interface';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+	async saveFiles(
+		files: Express.Multer.File[],
+		folder: string = 'default'
+	): Promise<IFileResponse[]> {
+		const uploadFolder = `${path}/uploads/${folder}`;
+		await ensureDir(uploadFolder);
+
+		const result: IFileResponse[] = await Promise.all(
+			files.map(async (file) => {
+				await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer);
+
+				return {
+					url: `/uploads/${folder}/${file.originalname}`,
+					name: file.originalname,
+				};
+			})
+		);
+
+		return result;
+	}
+}
