feabf6d8279851f075500d007227c15d07f96f3e
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-11 20:37:30 +0300

    add: added libs for files-entity

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { path } from 'app-root-path';
 
 @Module({
-  controllers: [FilesController],
-  providers: [FilesService],
+	controllers: [FilesController],
+	providers: [FilesService],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: 'uploads',
+		}),
+	],
 })
 export class FilesModule {}
