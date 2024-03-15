e78a645b56ac786279bd47bdc26a9e9742cf509e
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-15 17:18:45 +0300

    add: realized common controllers for movies

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
+
+  @Get('by-slug/:slug')
+  async getBySlug(@Param('slug') slug: string) {
+    return this.moviesService.getBySlug(slug);
+  }
+
+  @Get() 
+  async getAll(@Query('searchTerm') searchTerm?: string) {
+    return this.moviesService.getAll(searchTerm);
+  }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from 'nestjs-typegoose';
+import { MoviesEntity } from './movies.entity';
+import { ModelType } from '@typegoose/typegoose/lib/types';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+  constructor(
+    @InjectModel(MoviesEntity)
+    private readonly moviesEntity: ModelType<MoviesEntity>
+  ) {}
+}
