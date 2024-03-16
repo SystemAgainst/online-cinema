cf7807784a4acf2bca14cca935da5a8c73a03ac7
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-16 11:43:21 +0300

    add: controllers for sorting movies

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Get, HttpCode, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Put, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
 import { Types } from 'mongoose';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -31,8 +33,46 @@ export class MoviesController {
     return this.moviesService.getAll(searchTerm);
   }
 
+  @Get('/most-popular')
+	async getMostPopular() {
+		return this.moviesService.getMostPopular()
+	}
+
+  @Post('/update-count-opened')
+	@HttpCode(200)
+	async updateCountOpened(@Body('slug') slug: string) {
+		return this.moviesService.updateCountOpened(slug)
+	}
+
   @Get()
   async getById(@Param('id') id: string) {
     return this.moviesService.getById(id);
   }
+
+  @Post()
+	@HttpCode(200)
+	@Auth('admin')
+	async create() {
+		return this.moviesService.create()
+	}
+
+  @UsePipes(new ValidationPipe())
+	@Put(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async update(
+		@Param('id', IdValidationPipe) id: string,
+		@Body() dto: CreateMovieDto
+	) {
+		const updateMovie = await this.moviesService.update(id, dto)
+		if (!updateMovie) throw new NotFoundException('Movie not found')
+		return updateMovie
+	}
+
+	@Delete(':id')
+	@Auth('admin')
+	async delete(@Param('id', IdValidationPipe) id: string) {
+		const deletedDoc = await this.moviesService.delete(id)
+		if (!deletedDoc) throw new NotFoundException('Movie not found')
+	}
 }
