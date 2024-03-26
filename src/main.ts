e3cb05ff45af7fe1e6afa1609be48c9e4b16d67e
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-03-26 16:34:45 +0300

    add: added cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as process from 'process';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.enableCors({ origin: 'http://localhost:3000' });
 	app.setGlobalPrefix('api/v1');
 	await app.listen(process.env.PORT);
 }
